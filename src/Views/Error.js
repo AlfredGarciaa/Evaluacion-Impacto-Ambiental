9f72598e82976ca4b20f6079006e33d197ab34a2
Alfred <alfred.garcia@ucb.edu.bo>
2023-07-24 18:41:45 -0400

    404 NOT FOUNDED

--- src/Views/Error.js
+++ src/Views/Error.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "../Styles/Error.css";
+import sadFace from '../Assets/sadFace.png';
 
 const Error = () => {
   const links = [
@@ -8,15 +9,18 @@ const Error = () => {
   ];
 
   return (
-    <div className="error-container">
-      <h1 className="error-title">404 - Page Not Found</h1>
-      <p className="error-message">The requested page does not exist.</p>
-      {links.map((link) => (
-        <React.Fragment key={link.path}>
-          <Link to={link.path} className="error-link">Go back to {link.label}</Link>
-          <br />
-        </React.Fragment>
-      ))}
+    <div className="error-page">
+      <img src={sadFace} alt="Sad Face" className="sad-face" />
+      <div className="error-container">
+        <h1 className="error-title">404 - Page Not Found</h1>
+        <p className="error-message">The requested page does not exist.</p>
+        {links.map((link) => (
+          <React.Fragment key={link.path}>
+            <Link to={link.path} className="error-link">Go back to {link.label}</Link>
+            <br />
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
