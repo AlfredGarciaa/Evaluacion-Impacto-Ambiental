6081a189e06a41975957b7f36b8351237ccdc716
Alfred <alfred.garcia@ucb.edu.bo>
2023-07-24 18:18:32 -0400

    Found Page Negative

--- src/Views/Error.js
+++ src/Views/Error.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../Styles/Error.css";
 
 const Error = () => {
   const links = [
@@ -7,12 +8,12 @@ const Error = () => {
   ];
 
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The requested page does not exist.</p>
+    <div className="error-container">
+      <h1 className="error-title">404 - Page Not Found</h1>
+      <p className="error-message">The requested page does not exist.</p>
       {links.map((link) => (
         <React.Fragment key={link.path}>
-          <Link to={link.path}>Go back to {link.label}</Link>
+          <Link to={link.path} className="error-link">Go back to {link.label}</Link>
           <br />
         </React.Fragment>
       ))}
