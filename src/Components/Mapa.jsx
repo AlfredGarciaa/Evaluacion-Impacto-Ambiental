bd2590a25a472d90cc33551c10dc717b5d626329
Alfred <alfred.garcia@ucb.edu.bo>
2023-07-26 17:12:06 -0400

    Geolocalizador terminado

--- src/Components/Mapa.jsx
+++ src/Components/Mapa.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../Styles/Mapa.css';
+import L from 'leaflet'; 
+import customIcon from '../Assets/customIcon.png'; 
+
 
 const Mapa = () => {
-  const cochabambaCoords = [-17.3935, -66.1571];
+  const buenosAiresCoords = [-34.6132, -58.3772];
   const [currentLocation, setCurrentLocation] = useState(null);
+  const mapRef = useRef();
 
   const handleGetLocationClick = () => {
     if ("geolocation" in navigator) {
@@ -13,6 +17,8 @@ const Mapa = () => {
         (position) => {
           const { latitude, longitude } = position.coords;
           setCurrentLocation([latitude, longitude]);
+
+          mapRef.current.setView([latitude, longitude], 13);
         },
         (error) => {
           console.error("Error getting geolocation:", error);
@@ -23,27 +29,45 @@ const Mapa = () => {
     }
   };
 
+    const customMarkerIcon = new L.Icon({
+        iconUrl: customIcon,
+        iconSize: [60, 60], 
+        iconAnchor: [16, 32], 
+    });
+
   return (
     <div className="mapa-container">
+
       <h1 className="color-text-mapa">
         <i className="fa-solid fa-map"></i>
-        Ubicacion
+        Mapa
       </h1>
       <div style={{ height: '200px', width: '500px' }}>
-        <MapContainer center={cochabambaCoords} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <MapContainer ref={mapRef} center={currentLocation || buenosAiresCoords} zoom={11} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {currentLocation && (
-            <Marker position={currentLocation}>
+            <Marker position={currentLocation} icon={customMarkerIcon}> 
               <Popup>
-                Tu ubicación actual
+                Tu ubicación actual!
               </Popup>
             </Marker>
           )}
+
+            <Marker position={buenosAiresCoords} icon={customMarkerIcon}>
+                <Popup>
+                    Buenos Aires, Argentina!
+                </Popup>
+            </Marker>
         </MapContainer>
       </div>
-      <button onClick={handleGetLocationClick}>Obtener mi ubicación</button>
+      <div className="button-container">
+        <button onClick={handleGetLocationClick} className="pulse-button">
+            Ubicacion Actual
+        </button>
+      </div>
+
     </div>
   );
 };
