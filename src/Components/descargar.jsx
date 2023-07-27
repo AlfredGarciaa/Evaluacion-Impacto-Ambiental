33faf43245dd843a43062f026531cf98d30c8274
Alfred <alfred.garcia@ucb.edu.bo>
2023-07-27 02:41:53 -0400

    ENTREGA FINAL

--- src/Components/descargar.jsx
+++ src/Components/descargar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../Styles/Descargar.css';
 
-const Descargar = () => (
-  <div className="descargar-container">
-    <h>
+const Descargar = () => {
+  return (
+    <div className="descargar-container">
+      <a href="https://docs.google.com/spreadsheets/d/18lJbPdZLoz6aT1FpBh5mdMZM_Jo0bFxn/edit?usp=sharing&ouid=115374638990980240919&rtpof=true&sd=true">
         Descargar EXCEL
-    </h>
-  </div>
-);
+      </a>
+    </div>
+  );
+};
 
 export default Descargar;
